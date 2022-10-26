2935e91d3b5225d793e065d2c210d83717fd3067
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2022-10-26 17:22:33 +0200

    Put the player back onto the ground when out of bounds

---
+++ Assets/Scripts/BoundingArea.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundingArea : MonoBehaviour
+{
+    public Vector3 SpawnPoint;
+
+    // If the player hits the bound, put them back to the spawn point
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = SpawnPoint;
+            other.attachedRigidbody.velocity = Vector3.zero;
+            other.attachedRigidbody.angularVelocity = Vector3.zero;
+        }
+    }
+}
