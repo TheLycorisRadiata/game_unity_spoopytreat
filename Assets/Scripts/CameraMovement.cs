c805691678907f5600db68382eef29814b27c196
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2022-10-27 22:03:17 +0200

    Scene uses models

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,8 +6,8 @@ public class CameraMovement : MonoBehaviour
 {
     private GameObject target;
     private Vector3 back;
-    private float height = 0.3f;
-    private float distance = 4f;
+    private float height = 0.5f;
+    private float distance = 2.4f;
 
     void Start()
     {
@@ -19,7 +19,7 @@ public class CameraMovement : MonoBehaviour
         // The camera follows the player in 3rd person
         back = -target.transform.forward;
         back.y = height;
-        gameObject.transform.position = target.transform.position + back * distance;
+        gameObject.transform.position = target.transform.position + back * 2f * distance;
         gameObject.transform.forward = target.transform.position - gameObject.transform.position;
     }
 }
