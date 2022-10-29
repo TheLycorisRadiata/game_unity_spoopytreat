113743c5bab966c1ec614a29229f0e17e3edc36e
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2022-10-30 00:29:54 +0200

    Fix camera so the target is horizontally in the center of the view and not towards the top

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,8 +6,9 @@ public class CameraMovement : MonoBehaviour
 {
     private GameObject target;
     private Vector3 back;
+    private Vector3 rotation;
     private float height = 0.5f;
-    private float distance = 2.4f;
+    private float distance = 4f;
 
     void Start()
     {
@@ -16,10 +17,17 @@ public class CameraMovement : MonoBehaviour
 
     void Update()
     {
-        // The camera follows the player in 3rd person
+        // The camera follows the target in 3rd person
         back = -target.transform.forward;
         back.y = height;
-        gameObject.transform.position = target.transform.position + back * 2f * distance;
+        gameObject.transform.position = target.transform.position + back * distance;
+
+        // The camera turns to the left or the right with the target
         gameObject.transform.forward = target.transform.position - gameObject.transform.position;
+        
+        // Fix the camera x rotation axis so the target is horizontally in the center of the view and not towards the top
+        rotation = gameObject.transform.rotation.eulerAngles;
+        rotation.x = 0;
+        gameObject.transform.rotation = Quaternion.Euler(rotation);
     }
 }
