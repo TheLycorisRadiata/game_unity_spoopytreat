8fa6af4690562fde38150819dd8591a739b7d455
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2022-11-15 13:40:55 +0100

    Add audio mixer groups to used sounds: "Music", "Ambience" and "Effects" + "Master" to the audio manager

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -28,8 +28,7 @@ public class AudioManager : MonoBehaviour
 			s.source = gameObject.AddComponent<AudioSource>();
 			s.source.clip = s.clip;
 			s.source.loop = s.loop;
-
-			s.source.outputAudioMixerGroup = mixerGroup;
+			s.source.outputAudioMixerGroup = s.mixerGroup != null ? s.mixerGroup : mixerGroup;
 		}
 	}
 
