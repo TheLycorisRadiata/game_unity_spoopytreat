e7cfc0f930015ca09fbc4185a58a163446a08779
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2022-12-12 22:12:50 +0100

    Leave debug log for bounding area

--- Assets/Scripts/BoundingArea.cs
+++ Assets/Scripts/BoundingArea.cs
@@ -10,11 +10,12 @@ public class BoundingArea : MonoBehaviour
         spawnPoint = GameObject.FindGameObjectWithTag("Player").transform.position;
         // And add +2 to the y axis to make sure the spawn point is above ground
         spawnPoint = new Vector3(spawnPoint.x, spawnPoint.y + 2f, spawnPoint.z);
+        Debug.Log("Fix script: BoundingArea - The spawn point is correct, but the object's position isn't updated");
     }
 
     // If an object hits the bound, put it back to the spawn point
     private static void OnTriggerExit(Collider other)
     {
-        other.transform.position = spawnPoint;
+        other.gameObject.transform.position = spawnPoint;
     }
 }
